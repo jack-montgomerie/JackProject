183f567c0ae038d34516fedda46ac00963f99a5b
jack-montgomerie <136520333+jack-montgomerie@users.noreply.github.com>
2023-06-19 11:18:35 -0400

    Committing

---
+++ jackProject.js
@@ -0,0 +1,7 @@
+function changeImage() {
+  document.getElementById("twitter-logo").src = "2021 Twitter logo - white.png";
+}
+
+function resetImage() {
+  document.getElementById("twitter-logo").src = "2021 Twitter logo - black.png";
+}
